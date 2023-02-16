b2cafebfae1148e944b8e9d61a018766e0870e8b
Edwin Ichura <eduichura69@gmail.com>
2023-02-16 12:34:04 -0800

    a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line, using puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#iclude <stdio.h>
+
+/**
+ * main -Start point
+ *
+ * Description - Print text with printf function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
