93a21bb0cf4ede65abdc1cb5baae6bd5d7f4e248
Edwin Ichura <eduichura69@gmail.com>
2023-02-17 15:17:41 -0800

    numbers 0-9 with spaces and commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ *
+ * Description - Prints numbers 0-9 with commas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
