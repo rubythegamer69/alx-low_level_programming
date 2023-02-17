d3ce53c2f0c2682d0a7fe746b77e3f7b340f6237
Edwin Ichura <eduichura69@gmail.com>
2023-02-17 05:19:48 -0800

    Print lowercase characters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Start point
+ *
+ * Description - Print lowercase alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		printf("\n");
+	}
+
+	return (0);
+}
