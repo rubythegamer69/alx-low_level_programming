dd0c162459b0d906d9262dcc76a6ddf4302b1c08
Edwin Ichura <eduichura69@gmail.com>
2023-02-17 04:56:27 -0800

    Evaluate last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Start point
+ *
+ * Description - evaluate the last digit of a number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, b;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	b = n % 10;
+
+	if (b > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, b);
+	}
+	else if (b == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, b);
+	}
+	else
+		printf("Last digit of %d is %d and is less tha 6 and not 0\n", n, b);
+	return (0);
+}
