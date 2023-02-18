73ba62a8015c0a3da9431c46e7f08a2a157bfede
Edwin Ichura <eduichura69@gmail.com>
2023-02-17 16:05:16 -0800

    prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -32,7 +32,7 @@ int main(void)
 						putchar(' ');
 						putchar(j);
 						putchar(i);
-						if (l == 57 && k == 56 && j == 57 && i == 57)
+					if (l == 57 && k == 56 && j == 57 && i == 57)
 							break;
 						putchar(',');
 						putchar(' ');
