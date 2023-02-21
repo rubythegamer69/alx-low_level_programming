20c6228091a9a58242957b757eb72b8384035705
Edwin Ichura <eduichura69@gmail.com>
2023-02-21 11:51:11 -0800

    prints alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - starting point.
+ *
+ * description: prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	for ( a = 97; a < 123; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+
+	return (0);
+}
