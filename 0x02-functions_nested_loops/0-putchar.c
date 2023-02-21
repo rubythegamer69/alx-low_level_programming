f1307b5165501f89393731b18103844a02012d52
Edwin Ichura <eduichura69@gmail.com>
2023-02-21 08:02:27 -0800

    a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * Main - starting point
+ *
+ * Descriprion - prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
