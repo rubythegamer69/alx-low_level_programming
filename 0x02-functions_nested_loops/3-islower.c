12ed2edc1ce51c3ddbe7a1f0e3ca6592c2513056
Edwin Ichura <eduichura69@gmail.com>
2023-02-21 12:24:13 -0800

    Evaluate alphabets

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+/**
+ * _islower - evaluates alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	int a;
+
+	if (a >= 97 && a <= 122)
+	{
+		return (1);
+	} else
+		return (0);
+}
