9b5b65dc1a6b98c293f0a23d068fee9e2b6cf811
Edwin Ichura <eduichura69@gmail.com>
2023-02-23 12:23:39 -0800

    A function that prints numbers 0-14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,11 @@
-# - prints numbers between 0 to 14
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers between 0 to 14
  * 10 times.
-* Return: no return.
+ * Return: no return.
  */
+
 void more_numbers(void)
 {
 	int i, ch;
