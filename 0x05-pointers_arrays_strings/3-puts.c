d4a841bf5fae2206760cf991ad256dbb9afb83a1
Edwin Ichura <eduichura69@gmail.com>
2023-02-27 10:56:15 -0800

    prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _puts -prints a string, followed by a new line
+ *
+ * @str: input string
+ *
+ * Return: none
+ */
+
+void _puts(char *str)
+{
+	int i, j;
+
+	i = _strlen(str);
+	for (j = 0; j < i; j++)
+	{
+		if (str[j] == '\0')
+		{
+			_putchar(str[j]);
+		}
+		else
+		{
+			_putchar('\n');
+			break;
+		}
+	}
