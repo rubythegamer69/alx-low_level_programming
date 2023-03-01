11f07cf32d67a9341877074142f75299a2d78c79
Edwin Ichura <eduichura69@gmail.com>
2023-03-01 11:50:05 -0800

    using separators

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * cap_string - capitalizes all words of a string
  * @s: input string.
@@ -8,16 +9,16 @@
 char *cap_string(char *s)
 {
 	int a = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + a) >= 97 && *(s + a) <= 122)
 		*(s + a) = *(s + a) - 32;
-	count++;
+	a++;
 	while (*(s + a) != '\0')
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + a) == sep_words[i])
+			if (*(s + a) == separators[i])
 			{
 				if ((*(s + (a + 1)) >= 97) && (*(s + (a + 1)) <= 122))
 					*(s + (a + 1)) = *(s + (a + 1)) - 32;
