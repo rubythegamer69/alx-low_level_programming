5c0141b3fab8391b0261d5b6c27e0fe812e9d836
Edwin Ichura <eduichura69@gmail.com>
2023-04-10 14:21:57 -0700

    function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -7,6 +7,6 @@ void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 #endif
